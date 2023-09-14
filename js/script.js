27bb5bf061dd830eaaf1de3e57fd2c8bb7410363
Patryk Komar <dsofiks@gmail.com>
2023-09-14 21:36:52 +0200

    add code

--- js/script.js
+++ js/script.js
@@ -45,3 +45,44 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  function clearTitleList(){
+    document.querySelector(optTitleListSelector).innerHTML = "";
+  }
+  clearTitleList();
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
+  let html = '';
+  for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+    /* find the title element */
+
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTMLData = {id: articleId, title: articleTitle};
+    const linkHTML = templates.articleLink(linkHTMLData);
+    console.log(linkHTML);
+    /* insert link into titleList */
+   titleList.insertAdjacentHTML('beforebegin', linkHTML);
+   html = html + linkHTML;
+
+}
+
+generateTitleLinks();
+
+
+

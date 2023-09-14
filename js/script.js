acf91ebfe35a0ae7893a5d46ab0312cd1be0402e
Patryk Komar <dsofiks@gmail.com>
2023-09-14 18:07:47 +0200

    add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,16 +4,21 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
  }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  const clickedElement = this;
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){

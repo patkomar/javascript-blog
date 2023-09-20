bf29eb501baee7c8981ec2383eaa941bd45c9812
Patryk Komar <dsofiks@gmail.com>
2023-09-20 17:27:14 +0200

    add tag lists

--- js/script.js
+++ js/script.js
@@ -46,7 +46,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags .list';
 
   function generateTitleLinks(customSelector = ''){
 
@@ -86,6 +87,9 @@ const optArticleSelector = '.post',
  generateTitleLinks();
 
  function generateTags(){
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
+
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
 
@@ -115,9 +119,16 @@ const optArticleSelector = '.post',
       //const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
       const linkHTMLData = {id: tag , tagName: tag};
       const linkHTML = templates.tagLink(linkHTMLData);
+    
 
       /* add generated code to html variable */
       html = html + linkHTML
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){       
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+        
+      }
     /* END LOOP: for each tag */
     }
     /* insert HTML of all the links into the tags wrapper */
@@ -125,8 +136,13 @@ const optArticleSelector = '.post',
 
   /* END LOOP: for every article: */
 }
- }
-generateTags();
+/* [NEW] find list of tags in right column */
+const tagList = document.querySelector(optTagsListSelector);
+
+/* [NEW] add html from allTags to tagList */
+tagList.innerHTML = allTags.join(' ');
+}
+ generateTags();
 
 function tagClickHandler(event){
   /* prevent default action for this event */
